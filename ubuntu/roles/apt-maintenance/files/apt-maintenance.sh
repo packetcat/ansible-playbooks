26d749a670354330c1f74a4b39248ba8e2458824
staticsafe <jogajog@sadiqsaif.com>
2019-07-14 08:18:35 -0400

    make sure PATH is set in ubuntu/roles/apt-maintenance/files/apt-maintenance.sh

--- ubuntu/roles/apt-maintenance/files/apt-maintenance.sh
+++ ubuntu/roles/apt-maintenance/files/apt-maintenance.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-
+export PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games"
 # Update repositories
 apt-get update
 # Update packages
