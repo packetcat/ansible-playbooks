87e7452be6c4e95b4e150bb053dad70c5bb72a5c
staticsafe <jogajog@sadiqsaif.com>
2019-10-29 10:39:44 -0400

    Remove apt-maintenance.sh as it isn't needed anymore.

--- ubuntu/roles/apt-maintenance/files/apt-maintenance.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-export PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games"
-# Update repositories
-apt-get update
-# Update packages
-apt-get -y upgrade
-# Autoremove packages
-apt-get -y autoremove
